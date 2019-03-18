1d09af28d4c3edc69a7ac32cea6404eee8d8c0d0
Ekaterina Kadyrova <pin-ka@outlook.com>
2019-03-19 01:47:02 +0500

    Анимированный (с ошибками) персонаж бегает, и прыгает по платформам, враги генерятся. Взаимодействие врагов и персонажа пока не сделано.

---
+++ Lesson2/Assets/Scripts/walkingEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingEnemy : MonoBehaviour
+{
+    public float speed = 7f;
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Vector3 enemyScale = transform.localScale;
+            enemyScale.x *= -1;
+            transform.localScale = enemyScale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x * speed, GetComponent<Rigidbody2D>().velocity.y);
+
+    }
+}
